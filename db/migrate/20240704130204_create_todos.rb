bba9d64559df620b561ad9cf659dd6d94e600d91
xiangma0531 <shoma0531@outlook.com>
2024-07-04 22:03:51 +0900

    todosテーブル定義作成・マイグレーション

--- db/migrate/20240704130204_create_todos.rb
+++ db/migrate/20240704130204_create_todos.rb
@@ -1,7 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[6.0]
   def change
     create_table :todos do |t|
-
+      t.string :title, null: false
+      t.boolean :is_completed, default: false, null: false
       t.timestamps
     end
   end
