636b49919073623fa6f1b32141e44d93cc5f4f64
xiangma0531 <shoma0531@outlook.com>
2024-07-05 21:27:54 +0900

    Appコンポーネント修正2

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 import { useTodos } from './hooks/useTodos';
 
