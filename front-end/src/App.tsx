16bbec4f056fc16ef4f9a204ed6cb06818a73744
xiangma0531 <shoma0531@outlook.com>
2024-07-05 21:27:34 +0900

    Appコンポーネント修正

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTodos } from './hooks/useTodos';
 
 function App() {
+  const { getTodos, todos } = useTodos();
+
+  useEffect(() => getTodos, []);
+
   return (
     <div className="App">
-      Hello, React!!
+      <h1>Todoリスト</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.title}>
+            {todo.title}<br />
+            {todo.is_completed ? "完了" : "未着手"}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
